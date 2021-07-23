5d332984d33fe87d11c2284f50bbd4b522a01a54
danixzxc <daniil.grishel@gmail.com>
2021-07-23 15:33:20 +0300

    lesson 3, added light

--- Assets/Scripts/HpBar.cs
+++ Assets/Scripts/HpBar.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class HpBar : MonoBehaviour
 {
-    [SerializeField] public Texture2D healthbarBG;
-    [SerializeField] public Texture2D healthbarFill;
-    [SerializeField] public float health;
+    [SerializeField] private Texture2D healthbarBG;
+    [SerializeField] private Texture2D healthbarFill;
+    [SerializeField] private float health;
     private void OnGUI()
     {
         GUI.DrawTexture(new Rect(Screen.width * 0.5f, 15, 200, 20), healthbarBG);
--- Assets/Scripts/RgbaSlider.cs
+++ Assets/Scripts/RgbaSlider.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class RgbaSlider : MonoBehaviour
 {
     public Color myColor;
-    void OnGUI()
+    private void OnGUI()
     {
         myColor = RGBSlider(new Rect(0, 30, 200, 20), myColor);
     }
-    Color RGBSlider(Rect screenRect, Color rgba)
+    private Color RGBSlider(Rect screenRect, Color rgba)
     {
         rgba.r = LabelSlider(screenRect, rgba.r, 0.0f, 1.0f, "Red");
         screenRect.y += 20;
@@ -18,7 +18,7 @@ public class RgbaSlider : MonoBehaviour
         rgba.a = LabelSlider(screenRect, rgba.a, 0.0f, 1.0f, "Alpha");
         return rgba;
     }
-    float LabelSlider(Rect screenRect, float sliderValue, float sliderMinValue, float sliderMaxValue, string labelText)
+    private float LabelSlider(Rect screenRect, float sliderValue, float sliderMinValue, float sliderMaxValue, string labelText)
     {
         GUI.Label(screenRect, labelText);
         screenRect.x += screenRect.width;
